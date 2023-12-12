f05003d4318bbf26d27cb824541a9f5bfab09208
Karpov Kirill <maccdig@gmail.com>
2023-12-12 11:47:21 +0300

    Corrected indentations Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,12 +1,12 @@
 ﻿int AckermannFunction(int m, int n){
     if(m==0){
-        return n + 1;
+        return n+1;
     }
     if(m>0 && n==0){
-        return AckermannFunction(m - 1, 1);
+        return AckermannFunction(m-1,1);
     }
     else{
-        return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        return AckermannFunction(m-1, AckermannFunction(m,n-1));
     }
 }
 
